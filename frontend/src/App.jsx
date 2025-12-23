bedac7e400ac03c0747eecc1684bdde51fc8614d
amitmamgai2k <amitmamgai@gmail.com>
2025-12-23 13:33:23 +0530

    feat: Introduce article management with Redux slice, store setup, and user post management page.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,6 +27,7 @@ import UserPosts from "./pages/UserPosts/UserPosts";
 import UploadPost from "./pages/UserPosts/UploadPost";
 import ManagePost from "./pages/UserPosts/ManagePost";
 import ReadArticle from "./pages/UserPosts/RealArticle";
+import ManageArticle from "./pages/UserPosts/ManageArticle";
 import PrivacyPolicy from "./pages/miscellaneous/PrivacyPolicy";
 import TermsOfService from "./pages/miscellaneous/TermsOfService";
 import CookiePolicy from "./pages/miscellaneous/CookiePolicy";
@@ -76,6 +77,7 @@ function App() {
         <Route path="upload-post" element={<AuthGuard><UploadPost /></AuthGuard>} />
         <Route path="manage-posts" element={<AuthGuard><ManagePost /></AuthGuard>} />
         <Route path="read-article" element={<AuthGuard><ReadArticle /></AuthGuard>} />
+        <Route path="manage-article" element={<AuthGuard><ManageArticle /></AuthGuard>} />
 
         {/* Miscellaneous/Legal Pages */}
         <Route path="privacy" element={<PrivacyPolicy />} />
